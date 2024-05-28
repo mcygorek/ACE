bf3446c456108b9b4c30dcf26fcdb98d628b6c48
mcygorek <mo_cygorek@gmx.de>
2024-05-28 14:22:07 +0200

    single missing header file

---
+++ include/PCH.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <complex>
+
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+
+#include <stdexcept>
+#include <cstdlib>
+#include <cmath>
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues> 
+#include <Eigen/SVD>
+#include <Eigen/src/misc/RealSvd2x2.h>
+#include <unsupported/Eigen/MatrixFunctions>
+#include <unsupported/Eigen/KroneckerProduct>
+
