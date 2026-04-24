5ae1ce4e3dcf9dab34ae08e1aa056ffaa0f068f2
Moritz Cygorek <36968298+mcygorek@users.noreply.github.com>
2026-04-24 18:37:33 +0200

    Update setup.py

--- pybind/setup.py
+++ pybind/setup.py
@@ -3,5 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='ACE',
     version='0.0.0',
-    packages=['ACE']
+    install_requires=['numpy'],
+    #packages=find_packages(include=['ACE', 'ACE.*']),
+    #packages=['ACE'],
+    packages=find_packages(),
+    package_data={'shared lib': ['*.so']},
+    include_package_data=True,
+    zip_safe=False,
 )
